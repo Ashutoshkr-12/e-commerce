2767d51d9fda620dc53e8061658e46fe294b85bc
ASHUTOSH KUMAR <ashutoshkr.8920@gmail.com>
2025-09-28 17:41:43 +0530

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -89,7 +89,7 @@ const AddProduct = () => {
   </div>
 )}
 
-          </div>
+         
         </div>
         <div className="flex flex-col gap-1 max-w-md">
           <label className="text-base font-medium" htmlFor="product-name">
